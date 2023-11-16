db31f2d1d2e20ff709f10870e3b1baba9ccaa4e4
Philipp Kurpekov <p.kurpekov@gmail.com>
2023-11-16 17:08:05 +0300

    hw1 done

---
+++ hw1/hw1/src/test/java/CalculatorTest.java
@@ -0,0 +1,115 @@
+import org.example.Calculator.Calculator;
+import org.junit.jupiter.api.*;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CalculatorTest {
+    private Calculator calculator;
+//    public static void main(String[] args) {
+//
+//        // Проверка базового функционала с целыми числами:
+//        if (8 != Calculator.calculation(2, 6, '+')) {
+//            throw new AssertionError("Ошибка в методе");
+//        }
+//
+//        if (0 != Calculator.calculation(2, 2, '-')) {
+//            throw new AssertionError("Ошибка в методе");
+//        }
+//
+//        if (14 != Calculator.calculation(2, 7, '*')) {
+//            throw new AssertionError("Ошибка в методе");
+//        }
+//
+//        if (2 != Calculator.calculation(100, 50, '/')) {
+//            throw new AssertionError("Ошибка в методе");
+//        }
+//
+//        // Случаи с неправильными аргументами
+//        // аргумент operator типа char, должен вызывать исключение, если он получает не базовые символы (+-*/)
+//        // try {
+//        //     seminars.first.Calculator.Calculator.calculation(8, 4, '_');
+//        // } catch (IllegalStateException e) {
+//        //     if (!e.getMessage().equals("Unexpected value operator: _")) {
+//        //         throw new AssertionError("Ошибка в методе");
+//        //     }
+//        // }
+//
+//        // Проверка базового функционала с целыми числами, с использованием утверждений:
+//        assert 8 == Calculator.calculation(2, 6, '+');
+//        assert 0 == Calculator.calculation(2, 2, '-');
+//        assert 14 == Calculator.calculation(2, 7, '*');
+//        assert 2 == Calculator.calculation(100, 50, '/');
+//
+//
+//        // Проверка базового функционала с целыми числами, с использованием утверждений AssertJ:
+////        assertThat(Calculator.calculation(2, 6, '+')).isEqualTo(8);
+////        assertThat(Calculator.calculation(2, 2, '-')).isEqualTo(0);
+////        assertThat(Calculator.calculation(2, 7, '*')).isEqualTo(14);
+////        assertThat(Calculator.calculation(100, 50, '/')).isEqualTo(2);
+//
+//        // Проверка ожидаемого исключения, с использованием утверждений AssertJ:
+//        assertThatThrownBy(() ->
+//                Calculator.calculation(8, 4, '_')
+//        ).isInstanceOf(IllegalStateException.class);
+//
+////        System.out.println(Calculator.calculation(2_147_483_647, 1, '+')); // integer overflow
+////        System.out.println(Calculator.squareRootExtraction(169));
+//
+//        // Примерные решения домашних заданий из 1 лекции:
+//
+//        // HW1.1: Придумайте и опишите (можно в псевдокоде) функцию извлечения корня и
+//        // необходимые проверки для него используя граничные случаи
+//        // assertThatThrownBy(() ->
+//        //         seminars.first.Calculator.Calculator.squareRootExtraction(0, 1, -1)
+//        // ).isInstanceOf(SomeStateException.class);
+//
+//        // HW1.2: Как будет выглядеть проверка для случая деления на ноль? (с использованием AssertJ)
+//         assertThatThrownBy(() ->
+//                seminars.first.Calculator.Calculator.calculation(5, 0, '/')
+//         ).isInstanceOf(ArithmeticException.class);
+//
+//        // HW1.3: Сравните одну и ту же проверку с использованием условий, ассертов, AssertJ
+//        // в каком случае стандартное сообщение об ошибке будет более информативным?
+//        // if (0 != seminars.first.Calculator.Calculator.calculation(2, 6, '+')) {
+//        //     throw new AssertionError("Ошибка в методе");
+//        // }
+//        //   assert 0 == seminars.first.Calculator.Calculator.calculation(2, 6, '+');
+//        //    assertThat(seminars.first.Calculator.Calculator.calculation(2, 6, '+')).isEqualTo(0);
+//    }
+public static void main(String[] args) {
+    assertThatThrownBy(() -> Calculator.calculatingDiscount(-40.5, -60))
+            .isInstanceOf(ArithmeticException.class);
+}
+    @BeforeAll
+    public static void initAll() {
+// Здесь можно, например, подключиться к базе данных
+    }
+
+    @AfterAll
+    public static void tearDownAll() {
+// Закрытие соединения с базой данных
+    }
+
+    @BeforeEach
+    public void init() {
+        calculator = new Calculator(); // Инициализация перед каждым тестом
+    }
+
+    @AfterEach
+    public void tearDown() {
+        calculator = null; // Очистка после каждого теста
+    }
+
+    @Test
+    @DisplayName("Проверка расчета скидки")
+    public void testDiscountCalc()
+    {Assertions.assertEquals(38.5, calculator.calculatingDiscount(40.5, 2));
+    }
+    @Test
+    @DisplayName("Проверка на недопустимые аргументы")
+    public void testDiscountArgs() {
+        assertThrows(ArithmeticException.class, () ->
+                Calculator.calculatingDiscount(-40.5, -60));
+    }
+}
